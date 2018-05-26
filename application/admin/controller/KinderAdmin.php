79eb32c17eef49dd77756d2b9f96e328cfc8a57d
calvintsang <1056769938@qq.com>
2018-05-26 14:29:37 +0800

    增加幼儿园管理员控制器

---
+++ application/admin/controller/KinderAdmin.php
@@ -0,0 +1,11 @@
+<?php
+namespace app\admin\controller;
+
+class KinderAdmin EXTENDS BaseController
+{
+	public function index($kid)
+	{
+		# code...
+		this->fetch();
+	}
+}
--- build.php
+++ build.php
@@ -13,13 +13,11 @@ return [
     // 生成应用公共文件
     '__file__' => ['common.php', 'config.php', 'database.php'],
 
-    // 定义demo模块的自动生成 （按照实际定义的文件名生成）
     'admin'     => [
         '__file__'   => ['common.php'],
         '__dir__'    => ['behavior', 'controller', 'model', 'view'],
-        'controller' => ['Index', 'Kindergarten', 'PlayGround','Gallery','Play'],
-        'model'      => ['Kindergarten', 'PlayGround','Gallery','Play'],
+        'controller' => ['Index', 'Kindergarten', 'PlayGround','Gallery','Play','KinderAdmin'],
+        'model'      => ['Kindergarten', 'PlayGround','Gallery','Play','KinderAdmin'],
         'view'       => ['index/index'],
     ],
-    // 其他更多的模块定义
 ];
