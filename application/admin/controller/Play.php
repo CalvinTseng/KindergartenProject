d757305dfa07e48ddb3d82c2b1990a07431b6bb2
CXIANSHENG <457130005@qq.com>
2018-05-26 16:00:02 +0800

    未完成的添加功能

--- application/admin/controller/Play.php
+++ application/admin/controller/Play.php
@@ -21,4 +21,30 @@ class Play extends BaseController
 		$this->success('删除成功');
 	}else $this->error('删除失败');
 	}
+	public function addPlay()
+	{
+		return $this->fetch('addPlay');
+	}
+	public function doAddPlay()
+	{
+		# code..
+		$playname=$_POST["playname"];
+		$adminname=$_POST["adminname"];
+		$adminpass=$_POST["adminpass"];
+		$playdata=["pname"=>$playname];
+		if(Db::name("幼儿园信息")->insert($playdata)){
+			$map['pname']=$playname;
+			$play=Db::name("幼儿园信息")->where('pname',$playname)->select();
+			// dump($kinder[0]['kid']);
+			$admindata=["pid"=>$play[0]['pid'],"padacc"=>$adminname,"padpass"=>md5($adminpass)];
+			if(Db::name("管理员")->insert($playadmindata)){
+				$this->success("添加用户成功",'Play/index');
+			}else{
+				$play=Db::name("游乐场信息")->where('pname',$playname)->delete();
+				$this->error("添加用户失败");}
+		}else{
+			$this->error("添加用户失败");
+		}
+
+	}
 }
