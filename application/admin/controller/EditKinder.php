e62335c84357d8e58a7b7703bcbab2d0f314c90a
calvintsang <1056769938@qq.com>
2018-05-27 17:43:25 +0800

    添加幼儿园修改功能

--- application/admin/controller/EditKinder.php
+++ application/admin/controller/EditKinder.php
@@ -6,20 +6,34 @@ use \think\Session;
 
 class EditKinder EXTENDS BaseController
 {
+	public $kidid;
 	public function index($kid)
 	{
 		$kinder=Db::name("幼儿园信息")->select($kid);
 		$this->assign('kinder',$kinder);
 	return	$this->fetch();
 	}
-	public function edit($kid)
-	{
-		# code...
-		
-	}
 	public function update()
 	{
-		# code...
 		
+		# code...
+		if(request()->isPost()){
+		$kname=input("param.kindername");
+		$ktype=input("param.kindertype");
+		$content = input('post.content');
+		$receive = input("param.receive");
+		$opendate = input("param.opendate");
+		$jd  = input("param.jd");
+		$wd = input("param.wd");
+		$address = input("param.address");
+		$data=["kid"=>Session::get("KidForEdit"),"kname"=>$kname,"type"=>$ktype,"kintro"=>$content,"receive"=>$receive,"opendate"=>$opendate,"jd"=>$jd,"wd"=>$wd,"address"=>$address];
+		if(Db::table("幼儿园信息")->update($data)){
+			$this->success("修改成功");
+		}else{
+			$this->error("修改失败");
+		}
+		}
+
 	}
+
 }
--- application/admin/controller/KinderAdmin.php
+++ application/admin/controller/KinderAdmin.php
@@ -9,8 +9,9 @@ class KinderAdmin EXTENDS BaseController
 	public function index($kid)
 	{
 		# code...
+		Session::set("KidForEdit",$kid);
 		$kinder=Db::name("幼儿园信息")->where("kid",$kid)->select();
-//		dump($kinder);
+		dump(Session::get("KidForEdit"));
 		$this->assign('kinderinfo',$kinder);
 		return	$this->fetch();
 	}
