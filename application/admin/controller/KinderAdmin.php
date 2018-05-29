5cc81496f5baff98f7c565b0b6ea6b9c4f8126e0
yrx1 <1106685179@qq.com>
2018-05-29 15:25:00 +0800

    no
    no

--- application/admin/controller/KinderAdmin.php
+++ application/admin/controller/KinderAdmin.php
@@ -11,7 +11,6 @@ class KinderAdmin EXTENDS BaseController
 		# code...
 		Session::set("KidForEdit",$kid);
 		$kinder=Db::name("幼儿园信息")->where("kid",$kid)->select();
-		dump(Session::get("KidForEdit"));
 		$this->assign('kinderinfo',$kinder);
 		return	$this->fetch();
 	}
