ce3bcb18e1b60525c6e3df28e1b8590b6837855b
CXIANSHENG <457130005@qq.com>
2018-05-30 11:16:10 +0800

    游乐场修改

--- application/admin/controller/Play.php
+++ application/admin/controller/Play.php
@@ -48,6 +48,51 @@ class Play extends BaseController
 		}
 	}
 	
+	public function edit(){
+		$pid=input('pid');
+		$play=Db::name("游乐场信息")->find($pid);
+		echo json_encode($play);
+	}
+	
+	public function update(){
+		$file = request()->file('ppic');
+    	$pic="";
+    	$ppic2=input('ppic2');//图片没改之前的名字
+    // 移动到框架应用根目录/public/uploads/ 目录下
+    if($file){
+        $info = $file->rule('uniqid')->validate(['size'=>3000000,'ext'=>'jpg,png,gif,jpeg'])->move(ROOT_PATH . 'public' . DS . 'static' . DS . 'posters');
+        if($info){
+            $pic=$info->getFilename();
+        }else{
+            // 上传失败获取错误信息
+            echo $file->getError();
+        }
+    }
+		
+		
+		//获取表单数据
+//		$pname=input('pname');
+		$data=[
+			'pname'=>input('pname'),
+			'pintro'=>input('pintro'),
+			'hittimes'=>0,
+			'ppic'=>empty($pic)?"ppic2":$pic,
+			'paddress'=>input('paddress')
+		];
+		if(!empty($pic)){
+			if(file_exists(ROOT_PATH . 'public' . DS . 'static' . DS . 'posters'.DS.$ppic2)){
+				unlink(ROOT_PATH . 'public' . DS . 'static' . DS . 'posters'.DS.$ppic2);
+			}
+		}
+		$pid=input('pid');
+		//修改数据库
+		if(Db::name('游乐场信息')->where("pid",$pid)->update($data)){
+				$this->success('修改游乐场信息成功');			
+		}else{
+			$this->error('修改添加游乐场信息失败');
+		}
+	}
+	
 	public function deleteplay($id)
 	{
 		if(Db::name("游乐场信息")->delete($id))
@@ -56,30 +101,5 @@ class Play extends BaseController
 		$this->success('删除成功');
 	}else $this->error('删除失败');
 	}
-//	public function addPlay()
-//	{
-//		return $this->fetch('addPlay');
-//	}
-//	public function doAddPlay()
-//	{
-//		# code..
-//		$playname=$_POST["playname"];
-//		$adminname=$_POST["adminname"];
-//		$adminpass=$_POST["adminpass"];
-//		$playdata=["pname"=>$playname];
-//		if(Db::name("幼儿园信息")->insert($playdata)){
-//			$map['pname']=$playname;
-//			$play=Db::name("幼儿园信息")->where('pname',$playname)->select();
-//			// dump($kinder[0]['kid']);
-//			$admindata=["pid"=>$play[0]['pid'],"padacc"=>$adminname,"padpass"=>md5($adminpass)];
-//			if(Db::name("管理员")->insert($playadmindata)){
-//				$this->success("添加用户成功",'Play/index');
-//			}else{
-//				$play=Db::name("游乐场信息")->where('pname',$playname)->delete();
-//				$this->error("添加用户失败");}
-//		}else{
-//			$this->error("添加用户失败");
-//		}
-//
-//	}
+
 }
