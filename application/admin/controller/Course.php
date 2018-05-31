0a5b5d093b8aefcf2f701602f4fae7f2e0e8756c
yrx1 <1106685179@qq.com>
2018-05-31 16:45:28 +0800

    hhh
    111

--- application/admin/controller/Course.php
+++ application/admin/controller/Course.php
@@ -12,4 +12,26 @@ class Course EXTENDS BaseController
 		$this->assign('course',$course);
 		return	$this->fetch();
 	}
+    public function edit($kid)
+    {
+        $course=Db::name("幼儿早教信息")->where("kid",$kid)->select();
+        $this->assign('course',$course);
+        return 	$this->fetch();
+    }
+    public function update()
+    {
+        # code...
+        if(request()->isPost()){
+            $cname=input("param.coursename");
+            $content = input('post.content');
+            $price=input('param.courseprice');
+            $cid=Db::name("幼儿早教信息")->where("kid",Session::get("KidForEdit"))->find();
+            $data=["cid"=>$cid['cid'],"cname"=>$cname,"kid"=>Session::get("KidForEdit"),"cintro"=>$content,"cprice"=>$price,"cpic"=>"1.jpg"];
+            if(Db::table("幼儿早教信息")->update($data)){
+                $this->redirect('Course/index',['kid'=>Session::get("KidForEdit")]);
+            }else{
+                $this->error("修改失败");
+            }
+        }
+    }
 }
--- application/admin/controller/News.php
+++ application/admin/controller/News.php
@@ -12,4 +12,26 @@ class News EXTENDS BaseController
 		$this->assign('news',$news);
 		return	$this->fetch();
 	}
+    public function edit($kid)
+    {
+        $news=Db::name("幼教资讯")->where("kid",$kid)->select();
+        $this->assign('news',$news);
+        return 	$this->fetch();
+    }
+    public function update()
+    {
+        # code...
+        if(request()->isPost()){
+            $ntitle=input("param.ntitle");
+            $nhittimes=input('param.nhittimes');
+            $content = input('post.content');
+            $yid=Db::name("幼教资讯")->where("kid",Session::get("KidForEdit"))->find();
+            $data=["nid"=>$yid['nid'],"ntitle"=>$ntitle,"kid"=>Session::get("KidForEdit"),"narticle"=>$content,"nhittimes"=>$nhittimes,"npic"=>"1.jpg"];
+            if(Db::table("幼教资讯")->update($data)){
+                $this->redirect('Course/index',['kid'=>Session::get("KidForEdit")]);
+            }else{
+                $this->error("修改失败");
+            }
+        }
+    }
 }
