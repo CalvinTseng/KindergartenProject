64dcb8963ae3479f1bdbe3e9d09f459f16a0ed6b
yrx1 <1106685179@qq.com>
2018-06-03 14:55:10 +0800

    新闻资讯
    111

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -10,8 +10,10 @@ class Index extends Controller
     {
     	$kinder = Db::name('幼儿园信息')->select();
     	$pinder = Db::name('游乐场信息')->select();
+    	$ninder = Db::name('幼教资讯')->select();
     	$this->assign('kinder',$kinder);
     	$this->assign('pinder',$pinder);
+    	$this->assign('ninder',$ninder);
     	return $this->fetch();
       }
 }
