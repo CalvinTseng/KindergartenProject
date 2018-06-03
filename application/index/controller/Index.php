cb353d4249505b4d4f0d4c907fce7f6fcc3271db
yrx1 <1106685179@qq.com>
2018-06-03 14:43:42 +0800

    游乐场名字
    111

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -9,7 +9,9 @@ class Index extends Controller
     public function index()
     {
     	$kinder = Db::name('幼儿园信息')->select();
+    	$pinder = Db::name('游乐场信息')->select();
     	$this->assign('kinder',$kinder);
+    	$this->assign('pinder',$pinder);
     	return $this->fetch();
       }
 }
