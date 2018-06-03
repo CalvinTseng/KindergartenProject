5f61c180cc7fe9c0df584da20fde7786f9e9da9d
yrx1 <1106685179@qq.com>
2018-06-03 14:10:38 +0800

    幼儿园名字
    111

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -8,6 +8,8 @@ class Index extends Controller
 {
     public function index()
     {
+    	$kinder = Db::name('幼儿园信息')->select();
+    	$this->assign('kinder',$kinder);
     	return $this->fetch();
       }
 }
