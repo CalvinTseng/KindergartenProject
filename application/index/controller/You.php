926eba73ef80c64930fc5882e99038ff821a3204
yrx1 <1106685179@qq.com>
2018-06-05 20:42:47 +0800

    啊啊啊啊啊啊啊
    啊啊啊啊啊

--- application/index/controller/You.php
+++ application/index/controller/You.php
@@ -9,7 +9,37 @@ class You extends Controller
     public function index($kid)
     {
     	$aa = Db::name('幼儿园信息')->find($kid);
+    	$bb = Db::name('留言')->select();
     	$this->assign('kcon',$aa);
+    	$this->assign('kcon1',$bb);
     	return $this->fetch();
       }
+     public function liuyan(){
+        $content = input('content');
+	  	$data=[	  	      
+	  	      'kid'=>input('kid'),
+	  	      'codetail'=>$content,
+	  	      'codata'=>date('Y-m-d H:i:s')	  	      	  	      
+	  	];
+	  	
+	  	if(Db::name('留言')->insert($data)){
+	  		$this->success('发表评论成功');
+	  	}else $this->error('发表评论失败');
+}
+
+public function jubao($juid){
+	
+	$con = Db::name('留言')->field('codetail')->find($juid);
+	dump($con);
+	
+	$data = [
+	    'jbcon'=>$con['codetail'],
+	    'coid'=>$juid
+	];
+	if(Db::name('举报')->insert($data)){
+				$this->success('成功');			
+		}else{
+			$this->error('失败');
+		}
+}
 }
--- public/static/ueditor/php/controller.php
+++ public/static/ueditor/php/controller.php
@@ -38,11 +38,6 @@ switch ($action) {
         $result = include("action_crawler.php");
         break;
 
-    /* 删除图片命令处理 */
-    case 'deleteimage':
-         $result = include('action_delete.php');
-         break;
-
     default:
         $result = json_encode(array(
             'state'=> '请求地址出错'
