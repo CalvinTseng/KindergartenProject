87ac07e4db73b4af87d272293ee1832d113b9a6a
calvintsang <1056769938@qq.com>
2018-06-06 11:01:02 +0800

    忽略和删除

--- application/admin/controller/Report.php
+++ application/admin/controller/Report.php
@@ -12,4 +12,17 @@ class report EXTENDS BaseController
 		$this->assign('report',$report);
 		return $this->fetch();
 	}
+
+	public function ignore($coid)
+	{
+		if($report=Db::name('举报')->where('coid',$coid)->delete()){
+			$this->success('忽略成功');
+		}else{$this->error('忽略失败');}
+	}
+	public function delete($coid)
+	{
+		if(Db::name('举报')->where('coid',$coid)->delete() && Db::name('留言')->delete($coid)){
+			$this->success('删除成功');
+		}else{$this->error('删除失败');}
+	}
 }
